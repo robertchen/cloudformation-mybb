13d3413c874c60d6756f415db47acfc468252c7b
Robert <itconsultant8@gmail.com>
2017-01-14 10:47:16 -0500

    update

---
+++ download_install_mybb.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+# Environment variables.
+ echo " Parameters: email: $MYBB_ADMINEMAIL domainname: $MYBB_DOMAINNAME dbname: $MYBB_DBNAME  dbuser: $MYBB_DBUSERNAME dbpwd: $MYBB_DBPASSWORD dbhostname: $MYBB_DBHOSTNAME  dbport: $MYBB_DBPORT" >> /tmp/mybb_install.log
+
+# Config
+CONFIG="./mybb-config"
+SOURCE="./mybb-source"
+TARGET="/var/www/html"
+
+# Clean-up.
+rm -rf "$TARGET"/*
+cp -r "$SOURCE"/* "$TARGET"/
+
+# Prepare and copy dynamic configuration files.
+sed -e "s/MYBB_ADMINEMAIL/${MYBB_ADMINEMAIL}/g" \
+    -e "s/MYBB_DOMAINNAME/${MYBB_DOMAINNAME}/g" \
+    "${CONFIG}/settings.php" > "${TARGET}/inc/settings.php"
+
+sed -e "s/MYBB_DBNAME/${MYBB_DBNAME}/g" \
+    -e "s/MYBB_DBUSERNAME/${MYBB_DBUSERNAME}/g" \
+    -e "s/MYBB_DBPASSWORD/${MYBB_DBPASSWORD}/g" \
+    -e "s/MYBB_DBHOSTNAME/${MYBB_DBHOSTNAME}/g" \
+    -e "s/MYBB_DBPORT/${MYBB_DBPORT}/g" \
+    "${CONFIG}/config.php" > "${TARGET}/inc/config.php"
+
+# Initialize database.
+sed -e "s/MYBB_ADMINEMAIL/${MYBB_ADMINEMAIL}/g" \
+    -e "s/MYBB_DOMAINNAME/${MYBB_DOMAINNAME}/g" \
+    "${CONFIG}/mybb.sql" | mysql \
+    --user="$MYBB_DBUSERNAME" \
+    --password="$MYBB_DBPASSWORD" \
+    --host="$MYBB_DBHOSTNAME" \
+    --port="$MYBB_DBPORT" \
+    --database="$MYBB_DBNAME" || echo "WE ASSUME DATA ALREADY EXISTS!"
+	
+	
+# Set proper ownership and permissions.
+cd "$TARGET"
+# chown www-data:www-data *
+chmod 666 inc/config.php inc/settings.php
+chmod 666 inc/languages/english/*.php inc/languages/english/admin/*.php
+
+# TODO: The "uploads/" path should be mounted on an S3 bucket.
+chmod 777 cache/ cache/themes/ uploads/ uploads/avatars/
+chmod 777 cache/ cache/themes/ uploads/ uploads/avatars/ admin/backups/
